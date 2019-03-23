a69d3e3e70360c97fa57bcf068fb6ca67fa71a25
Saulo Santos <saulo.santos@conductor.com.br>
2019-03-23 10:47:01 -0300

    Criado o repositorio, responsavel por executar operações banco de dados.

---
+++ src/main/java/br/com/saulo/store/repositorios/StoreRepositorio.java
@@ -0,0 +1,13 @@
+package br.com.saulo.store.repositorios;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.saulo.store.entidades.StoreEntidade;
+
+@Repository
+public interface StoreRepositorio extends JpaRepository<StoreEntidade, Long> {
+
+	StoreEntidade findById(long id);
+
+}
